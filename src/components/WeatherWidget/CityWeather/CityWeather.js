7f9ab0af917ad50260ae395726882c95adce92e6
antoine <antoine.lefrancq.manul@gmail.com>
2022-12-22 22:14:53 +0100

    :sparkles: adding new weather in dynamic with an API call

--- src/components/WeatherWidget/CityWeather/CityWeather.js
+++ src/components/WeatherWidget/CityWeather/CityWeather.js
@@ -1,15 +1,33 @@
-import PropTypes from 'prop-types'
-const CityWeather = ({city,zip})=>{
+import axios from 'axios';
+import PropTypes from 'prop-types';
+import {useEffect, useState} from 'react';
+
+
+const CityWeather = ({city,zip,country})=>{
+  const [temperature, setTemperature]= useState(null)
+
+  useEffect(()=>{
+    const apiKey=process.env.REACT_APP_API_KEY
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?zip=${zip},${country}&appid=${apiKey}&units=metric`)
+    .then((response)=>{
+        setTemperature(Math.round(response.data.main.temp))
+    })
+  }, [])
+
+
   return(
     <article className='cityWeather'>
         <h1 className='city'>{city}</h1>
         <h2 className='zip'>{zip}</h2>
-        <p className='temperature'>29°C</p>
+        <p className='temperature'>{temperature}° C</p>
     </article>
   )
 };
+
 CityWeather.propTypes={
     city:PropTypes.string.isRequired,
     zip:PropTypes.string.isRequired,
+    country:PropTypes.string.isRequired,
 }
+
 export default CityWeather
--- src/components/WeatherWidget/WeatherWidget.js
+++ src/components/WeatherWidget/WeatherWidget.js
@@ -4,8 +4,10 @@ import './WeatherWidget.css';
 function WeatherWidget() {
   return (
     <section className="WeatherWidget">
-      <CityWeather city="Le Moule" zip='97160' />
-      <CityWeather city="Paris" zip="75001" />
+      <CityWeather city="Le Moule" zip='97160' country="gp"/>
+      <CityWeather city="Paris" zip="75001" country="FR"/>
+      <CityWeather city="L'Isle-sur-la-Sorgue" zip='84800' country='FR' />
+      <CityWeather city="New York City" zip='10001' country='US' />
     </section>
   );
 }
